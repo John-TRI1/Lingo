8617051fb640645a5df68402d5392b339b035842
shelbs-prog <SHELBYCROSBY@YAHOO.COM>
2026-04-11 06:57:03 -0500

    Added Lingo header Added flashcard feature with word and sentence on front and back

--- app/src/main/java/com/example/lingo/MainActivity.kt
+++ app/src/main/java/com/example/lingo/MainActivity.kt
@@ -1,47 +1,41 @@
 package com.example.lingo
 
+
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lingo.ui.theme.LingoTheme
+import androidx.cardview.widget.CardView
+
+
+
+
 
 class MainActivity : ComponentActivity() {
+
+
+    private var showingFront = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            LingoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        val flashcard = findViewById<CardView>(R.id.flashcard)
+        val flashcardFront = findViewById<View>(R.id.flashcard_front)
+        val flashcardBack = findViewById<View>(R.id.flashcardBack)
+
+        flashcard.setOnClickListener {
+            showingFront = !showingFront
+            if (showingFront) {
+                flashcardFront.visibility = View.VISIBLE
+                flashcardBack.visibility = View.GONE
+            } else {
+                flashcardFront.visibility = View.GONE
+                flashcardBack.visibility = View.VISIBLE
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LingoTheme {
-        Greeting("Android")
     }
 }
